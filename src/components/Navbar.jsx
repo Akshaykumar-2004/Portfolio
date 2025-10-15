a48f59c54b4b1e92a629f67463e9274a3b130c3f
AkshaykumarT A <akshayotp087@gmail.com>
2025-10-15 11:42:51 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ export default function Navbar() {
         </ul>
 
         <motion.a
-          href="/assets/resume.pdf"
+          href="/assets/Akshayresume.pdf"
           download="Akshay-Resume.pdf"
           className="hidden relative lg:inline-block px-4 py-2 font-medium group"
         >
